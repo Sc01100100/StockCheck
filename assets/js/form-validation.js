d1f9d6125e9b08bf728588bb11efc7cd6a2a3135
nekowawolf <irukainu20@gmail.com>
2025-01-26 22:26:54 +0700

    feat: update form validation

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -10,6 +10,20 @@ const blockInvalidKeys = (inputId) => {
 
 blockInvalidKeys("item-stock");
 
+const incomeAmountInput = document.getElementById("amount");
+
+incomeAmountInput.addEventListener("keydown", (event) => {
+  if (
+    event.key === "-" ||
+    event.key === "+" ||
+    event.key === "e" ||
+    event.key === "E" ||
+    event.key === "."
+  ) {
+    event.preventDefault();
+  }
+});
+
 function restockInput(input) {
   input.value = input.value.replace(/[^0-9]/g, "");
 }
